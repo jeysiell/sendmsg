13c64b5bc73875c794f087d469663d78d76bfdfb
jeysiell <gambiarras13feliz@gmail.com>
2025-07-22 09:13:00 -0300

    Delete start.js

--- start.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-
-console.log("📦 Instalando dependências...");
-execSync('npm install', { stdio: 'inherit' });
-
-console.log("🚀 Iniciando o sistema...");
-execSync('node whatsapp.js', { stdio: 'inherit' }); // Altere index.js se necessário
