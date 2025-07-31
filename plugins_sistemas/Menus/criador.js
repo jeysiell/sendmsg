86e3365e771cdca87cf8ccd4c5592352e8876954
jeysiell <gambiarras13feliz@gmail.com>
2025-07-31 14:49:22 -0300

    clear

--- plugins_sistemas/Menus/criador.js
+++
@@ -1,9 +0,0 @@
-// criador.js
-const sendCriadorMessage = (from, sender, nomedele, wxt, NOME_BOT, info) => {
-const x = `_( Olá ${nomedele} )_\n\n*INFO CRIADOR:*\n Nome: _wilmo chang_\n WhatsApp: _+258840718221_\n País: _Moçambique_`;
-wxt.sendMessage(from, {text: `${x}`, contextInfo: {mentionedJid: [sender], externalAdReply: { showAdAttribution: true,  thumbnailUrl: 'https://ibb.co/Jw9V3ncX',  title: "CRIADOR", body: "© wilmochang " + NOME_BOT, previewType: "PHOTO"}}}, {quoted: info});
-};
-
-module.exports = sendCriadorMessage;
-
-// Copyright © 𝑊𝐼𝐿𝑀𝑂 𝐶𝐻𝐴𝑁𝐺 2025
